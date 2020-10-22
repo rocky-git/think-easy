88fb976fe0bdb1d9f782300ec3dcef73fd6ee32a
rocky <631670747@qq.com>
2020-10-22 16:13:07 +0800

    权限验证增加缓存

--- src/service/AdminService.php
+++ src/service/AdminService.php
@@ -8,6 +8,7 @@
 
 namespace thinkEasy\service;
 
+use think\facade\Cache;
 use thinkEasy\Service;
 
 /**
@@ -17,13 +18,13 @@ use thinkEasy\Service;
  */
 class AdminService extends Service
 {
+
     /**
      * 获取权限菜单
      */
     public function menus()
     {
         $menus = MenuService::instance()->all();
-
         if ($this->id() != config('admin.admin_auth_id')) {
             $pids = array_column($menus, 'pid');
             foreach ($menus as $key => $menu) {
@@ -48,7 +49,6 @@ class AdminService extends Service
                 }
             }
         }
-
         return MenuService::instance()->treeMenus($menus);
     }
     protected function findMenuChildren($menu_id,$menuList=[]){
@@ -80,7 +80,6 @@ class AdminService extends Service
         $method = strtolower($method);
         $node = strtolower($node);
         $ext = pathinfo($node, PATHINFO_EXTENSION);
-
         if(strpos($node,'edit.rest')){
             $node = preg_replace("/(.+)\/(.+)\/(.+)\/edit\.rest$/U","\\1/\\2/:id/edit.rest",$node);
         }elseif(strpos($node,'create.rest')){
@@ -89,9 +88,7 @@ class AdminService extends Service
 
             $node = preg_replace("/(.+)\/(.+)\/(.+)\.rest$/U","\\1/\\2/:id.rest",$node);
         }
-
         $permissions = $this->permissions();
-
         if(empty($method)){
             $rules = array_column($permissions, 'rule');
             if (in_array($node, $rules)) {
@@ -121,15 +118,19 @@ class AdminService extends Service
     {
         return TokenService::instance()->user();
     }
-
     /**
      * 获取权限节点
      * @return mixed
      */
     public function permissions()
     {
+        $permissionsKey = 'eadmin_permissions'.$this->id();
+        $newNodes = Cache::get($permissionsKey);
+        if($newNodes){
+            return $newNodes;
+        }
         $nodes = NodeService::instance()->all();
-        if($this->user()){
+        if($this->id()){
             $permissions = $this->user()->permissions();
         }else{
             $permissions = [];
@@ -146,10 +147,9 @@ class AdminService extends Service
                 $newNodes[] = $nodes[$key];
             }
         }
-
+        Cache::set($permissionsKey,$newNodes,5);
         return $newNodes;
     }
-
     /**
      * 获取用户角色组
      */
@@ -157,7 +157,6 @@ class AdminService extends Service
     {
         return $this->user()->roles;
     }
-
     /**
      * 获取当前登陆用户id
      * @return string
