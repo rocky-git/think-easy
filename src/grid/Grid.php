bcf6053c80953066e970162697ec35b0713d3ca7
rocky <631670747@qq.com>
2021-01-18 19:50:22 +0800

    删除关联静态判断

--- src/grid/Grid.php
+++ src/grid/Grid.php
@@ -622,7 +622,7 @@ EOF;
             if ($method->class == $className) {
                 $relation = $method->name;
                 $p = new \ReflectionMethod($method->class, $relation);
-                if ($p->getNumberOfParameters() == 0) {
+                if ($p->getNumberOfParameters() == 0 && !$p->isStatic()) {
                     if ($this->model->$relation() instanceof BelongsToMany) {
                         if ($deleteDatas === true) {
                             $deleteDatas = $this->model->select();
